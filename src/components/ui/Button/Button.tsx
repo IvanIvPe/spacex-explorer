0d19733472fab1c4c05bdb1deae1a9d6adda0009
IvanIvPe <pesicivan2002@gmail.com>
2026-02-03 14:16:08 +0100

    Remove unused forwardRef import from Button component

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from 'react';
+import React from 'react';
 import styles from './Button.module.css';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
